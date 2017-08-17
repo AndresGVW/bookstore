b4e79ea6202206e3a3321ad517b5e5fa684e96d7
AndresGVW <andresvonwalter@gmail.com>
2017-08-17 07:29:41 +0100

    Create tho Book Model gain, add books to the inventory

--- store/models.py
+++ store/models.py
@@ -5,5 +5,10 @@ from django.utils import timezone
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    description = models.TextField
-    publish_date = models.DateField(timezone.now)
+    description = models.TextField()
+    publish_date = models.DateField(default=timezone.now)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
